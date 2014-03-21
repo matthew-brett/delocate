800a38f69707a52b7836ae280728884dc9f56b91
Matthew Brett <matthew.brett@gmail.com>
2014-03-21 16:52:26 -0700

    BF: work out API for delocation, and test
    Make delocation a little less ambitious, in that it doesn't try to
    resolve links from copied libraries.

--- delocate/delocator.py
+++ delocate/delocator.py
@@ -1,13 +1,20 @@
-""" Routines to manipulate dynamic libaries in trees
+""" Routines to manipulate dynamic libraries in trees
 """
 
 from __future__ import division, print_function
 
 from os.path import (join as pjoin, split as psplit, abspath, dirname, basename,
-                     exists)
+                     exists, relpath)
+
+import shutil
+
+from .tools import add_rpath, set_install_name
+
+class DelocationError(Exception):
+    pass
 
 
-def delocate_tree_libs(lib_dict, lib_path, root_path = None):
+def delocate_tree_libs(lib_dict, lib_path, root_path):
     """ Move needed libraries in `lib_dict` into `lib_path`
 
     `lib_dict` has keys naming libraries required by the files in the
@@ -30,11 +37,56 @@ def delocate_tree_libs(lib_dict, lib_path, root_path = None):
         dictionary with (key, value) pairs of (install name, set of files in
         tree with install name)
     lib_path : str
-        path in which to store copies of libs referred to in keys of `lib_dict`
-    root_path : None or str, optional
-        If str, root directory of tree analyzed in `lib_dict`.  Any required
+        path in which to store copies of libs referred to in keys of
+        `lib_dict`.  Assumed to exist
+    root_path : str, optional
+        root directory of tree analyzed in `lib_dict`.  Any required
         library within the subtrees of `root_path` does not get copied, but
         libraries linking to it have links adjusted to use relative path to
-        this library. If None, always copy libraries.
+        this library.
+
+    Returns
+    -------
+    copied_libs : set
+        set of names of libraries copied into `lib_path`. Names are filenames
+        relative to `lib_path`
     """
-    pass
+    copied_libs = set()
+    delocated_libs = set()
+    copied_basenames = set()
+    # Test for errors first to avoid getting half-way through changing the tree
+    for required, requirings in lib_dict.items():
+        if required.startswith('@'): # assume @rpath etc are correct
+            continue
+        r_ed_base = basename(required)
+        if relpath(required, root_path).startswith('..'):
+            # Not local, plan to copy
+            if r_ed_base in copied_basenames:
+                raise DelocationError('Already planning to copy library with '
+                                      'same basename as: ' + r_ed_base)
+            if not exists(required):
+                raise DelocationError('library "{0}" does not exist'.format(
+                    required))
+            copied_libs.add(required)
+            copied_basenames.add(r_ed_base)
+        else: # Is local, plan to set relative loader_path
+            delocated_libs.add(required)
+    # Modify in place now that we've checked for errors
+    rpathed = set()
+    for required in copied_libs:
+        shutil.copy2(required, lib_path)
+        # Set rpath and install names for this copied library
+        for requiring in lib_dict[required]:
+            if requiring not in rpathed:
+                req_rel = relpath(lib_path, dirname(requiring))
+                add_rpath(requiring, '@loader_path/' + req_rel)
+                rpathed.add(requiring)
+            set_install_name(requiring, required,
+                             '@rpath/' + basename(required))
+    for required in delocated_libs:
+        # Set relative path for local library
+        for requiring in lib_dict[required]:
+            req_rel = relpath(required, dirname(requiring))
+            set_install_name(requiring, required,
+                             '@loader_path/' + req_rel)
+    return copied_libs
--- delocate/tests/test_delocate.py
+++ delocate/tests/test_delocate.py
@@ -4,15 +4,15 @@ from __future__ import division, print_function
 
 import os
 from os.path import (join as pjoin, split as psplit, abspath, dirname, basename,
-                     exists)
+                     exists, relpath)
 
 from subprocess import Popen, PIPE, check_call
 
-from ..delocator import delocate_tree_libs
+from ..delocator import delocate_tree_libs, DelocationError
 from ..tools import (tree_libs, get_install_names, get_rpaths,
                      set_install_name, back_tick)
 
-from ..tmpdirs import InTemporaryDirectory
+from ..tmpdirs import InTemporaryDirectory, InGivenDirectory
 
 from nose.tools import (assert_true, assert_false, assert_raises,
                         assert_equal, assert_not_equal)
@@ -51,59 +51,69 @@ def test_delocate_tree_libs():
     with InTemporaryDirectory() as tmpdir:
         # Copy libs into a temporary directory
         subtree = pjoin(tmpdir, 'subtree')
-        liba, libb, libc, test_lib, slibc, stest_lib = _make_libtree(subtree)
+        all_local_libs = _make_libtree(subtree)
+        liba, libb, libc, test_lib, slibc, stest_lib = all_local_libs
         lib_dict = tree_libs(subtree)
-        # By default we'll fail to find liba etc because of the relative paths
-        assert_raises(ValueError, delocate_tree_libs, lib_dict, 'dynlibs')
-        # Fixup the library names by setting absolute paths
-        for key, replacement in (('liba.dylib', liba),
-                                 ('libb.dylib', libb),
-                                 ('libc.dylib', libc)):
-            for using_lib in lib_dict[key]:
-                set_install_name(using_lib, key, replacement)
-        delocate_tree_libs(lib_dict, 'dynlibs')
-        # All the libraries linked to get copied
-        assert_equal(sorted(os.listdir('dynlibs')),
-                     ['libSystem.B.dylib', 'liba.dylib', 'libb.dylib',
-                      'libc.dylib', 'libstdc++.6.dylib'])
-        # All linking libraries now have a relative rpath and a link to the
-        # copied library via the rpath
-        for required, requirings in lib_dict.items():
-            rpathed = '@rpath/' + basename(required)
-            for requiring in requirings:
-                assert_equal(get_rpaths(requiring),
-                             ('@loader_path/../dynlibs',))
-                assert_true(rpathed in get_install_names(requiring))
-        # The copied library also links to local libraries via relative path
-        # Could be also be just @loader_path in the install names of course.
-        new_libb = pjoin(tmpdir, 'dynlibs', 'libb.dylib')
-        assert_equal(set(get_install_names(new_libb)),
-                     set(('@rpath/liba.dylib',
-                          '@rpath/libstdc++.6.dylib',
-                          '@rpath/libSystem.B.dylib')))
-        assert_equal(get_rpaths(new_libb), ('@loader_path',))
-    # Do the same, but with the option to keep libraries in the tree
-    with InTemporaryDirectory() as tmpdir:
-        subtree = pjoin(tmpdir, 'subtree')
-        liba, libb, libc, test_lib = _copy_libs(
-            [LIBA, LIBB, LIBC, TEST_LIB], subtree)
-        subsubtree = pjoin(subtree, 'further')
-        slibb, slibc, stest_lib = _copy_libs([libb, libc, test_lib], subsubtree)
-        lib_dict = tree_libs(subtree)
-        # Fixup the library names by setting absolute paths
-        for key, replacement in (('liba.dylib', liba),
-                                 ('libb.dylib', libb),
-                                 ('libc.dylib', libc)):
-            for using_lib in lib_dict[key]:
-                set_install_name(using_lib, key, replacement)
-        # Now do copy preserving in-tree libraries
-        delocate_tree_libs(lib_dict, 'dynlibs', 'subtree')
+        copy_dir = 'dynlibs'
+        os.makedirs(copy_dir)
+        copied = delocate_tree_libs(lib_dict, copy_dir, subtree)
         # Only the out-of-tree libraries get copied
-        ext_libs = ['libSystem.B.dylib', 'libstdc++.6.dylib']
-        assert_equal(sorted(os.listdir('dynlibs')), ext_libs)
-        for key in ext_libs:
-            for requiring in lib_dict[key]:
-                assert_equal(get_rpaths(requiring),
-                             ('@loader_path/../dynlibs',))
-                assert_true(rpathed in get_install_names(requiring))
-
+        ext_libs = set(('/usr/lib/libstdc++.6.dylib',
+                        '/usr/lib/libSystem.B.dylib'))
+        assert_equal(copied, ext_libs)
+        assert_equal(set(os.listdir(copy_dir)),
+                     set([basename(lib) for lib in ext_libs]))
+        # Libraries using the copied libraries now have an rpath pointing to
+        # the copied library directory, and rpath/libname as install names
+        to_exts = set(['@rpath/' + basename(elib) for elib in copied])
+        for lib in all_local_libs:
+            pathto_copies = relpath(copy_dir, dirname(lib))
+            assert_equal(get_rpaths(lib), ('@loader_path/' + pathto_copies,))
+            assert_true(to_exts <= set(get_install_names(lib)))
+        # Libraries now have a relative loader_path to their corresponding
+        # in-tree libraries
+        for requiring, using, rel_path in (
+            (libb, 'liba.dylib', ''),
+            (libc, 'liba.dylib', ''),
+            (libc, 'libb.dylib', ''),
+            (test_lib, 'libc.dylib', ''),
+            (slibc, 'liba.dylib', '../'),
+            (slibc, 'libb.dylib', '../'),
+            (stest_lib, 'libc.dylib', '')):
+            loader_path = '@loader_path/' + rel_path + using
+            assert_true(loader_path in get_install_names(requiring))
+        # Check test libs still work
+        back_tick([test_lib])
+        back_tick([stest_lib])
+        # Check case where all local libraries are out of tree
+        subtree2 = pjoin(tmpdir, 'subtree2')
+        liba, libb, libc, test_lib, slibc, stest_lib = _make_libtree(subtree2)
+        copy_dir2 = 'dynlibs2'
+        os.makedirs(copy_dir2)
+        # Trying to delocate where all local libraries appear to be
+        # out-of-tree will raise an error because of duplicate library names
+        lib_dict2 = tree_libs(subtree2)
+        assert_raises(DelocationError,
+                      delocate_tree_libs, lib_dict2, copy_dir2, '/tmp')
+        # Rename a library to make this work
+        new_slibc = pjoin(dirname(slibc), 'libc2.dylib')
+        os.rename(slibc, new_slibc)
+        # Tell test-lib about this
+        set_install_name(stest_lib, slibc, new_slibc)
+        slibc = new_slibc
+        # Confirm new test-lib still works
+        back_tick([stest_lib])
+        # Delocation now works
+        lib_dict2 = tree_libs(subtree2)
+        copied2 = delocate_tree_libs(lib_dict2, copy_dir2, '/tmp')
+        ext_local_libs = ext_libs | set([liba, libb, libc, slibc])
+        assert_equal(copied2, ext_local_libs)
+        assert_equal(set(os.listdir(copy_dir2)),
+                     set([basename(lib) for lib in ext_local_libs]))
+        # Libraries using the copied libraries now have an rpath pointing to
+        # the copied library directory, and rpath/libname as install names
+        all_local_libs = liba, libb, libc, test_lib, slibc, stest_lib
+        for lib in all_local_libs:
+            pathto_copies = relpath(copy_dir2, dirname(lib))
+            assert_equal(get_rpaths(lib), ('@loader_path/' + pathto_copies,))
+            assert_true(to_exts <= set(get_install_names(lib)))

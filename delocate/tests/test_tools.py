034e48b26f41fb2e3699a727434dd1c537675b4e
Matthew Brett <matthew.brett@gmail.com>
2014-03-24 16:35:02 -0700

    WIP: add tests for ensure_writable decorator

--- delocate/tests/test_tools.py
+++ delocate/tests/test_tools.py
@@ -1,7 +1,11 @@
 """ Test tools module """
 from __future__ import division, print_function
 
-from ..tools import back_tick
+import os
+
+from ..tools import back_tick, ensure_writable
+
+from ..tmpdirs import InTemporaryDirectory
 
 from nose.tools import assert_true, assert_false, assert_equal, assert_raises
 
@@ -13,3 +17,24 @@ def test_back_tick():
     assert_equal(back_tick(cmd, True, False), (b"Hello", b""))
     cmd = 'python -c "raise ValueError()"'
     assert_raises(RuntimeError, back_tick, cmd)
+
+
+def test_ensure_writable():
+    # Test ensure writable decorator
+    from ..tmpdirs import InGivenDirectory as InTemporaryDirectory
+    with InTemporaryDirectory():
+        with open('test.bin', 'wt') as fobj:
+            fobj.write('A line\n')
+        # Set to user rw, else r
+        os.chmod('test.bin', 0o644)
+        st = os.stat('test.bin')
+        @ensure_writable
+        def foo(fname):
+            with open(fname, 'a') as fobj:
+                fobj.write('Another line\n')
+        foo('test.bin')
+        assert_equal(os.stat('test.bin'), st)
+        os.chmod('test.bin', 0o444)
+        st = os.stat('test.bin')
+        foo('test.bin')
+        assert_equal(os.stat('test.bin'), st)

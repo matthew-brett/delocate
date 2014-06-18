ffc2d8ec9bdec14f31dc86812065c96b3b0c20af
Matthew Brett <matthew.brett@gmail.com>
2014-06-18 01:09:29 +0100

    Revert "RF: use setuptools for setup where possible"
    This reverts commit 3a0e45272c7c4acc82c2e4c4ac5fb6ec87f3bd24.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 import sys
 from os.path import join as pjoin
 
-try:
+# For some commands, use setuptools.
+if len(set(('develop', 'bdist_egg', 'bdist_rpm', 'bdist', 'bdist_dumb',
+            'install_egg_info', 'egg_info', 'easy_install', 'bdist_wheel',
+            'bdist_mpkg')).intersection(sys.argv)) > 0:
     import setuptools
-except ImportError:
-    pass
 
 from distutils.core import setup
 import versioneer

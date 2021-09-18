44e2ca4f761c821dc11d349feaf24e455680590b
Matthew Brett <matthew.brett@gmail.com>
2021-09-18 14:29:59 +0100

    Update to versioneer 0.20

--- delocate/__init__.py
+++ delocate/__init__.py
@@ -9,3 +9,6 @@ del get_versions
 
 __all__ = ("delocate_path", "delocate_wheel", "patch_wheel", "tree_libs",
            "wheel_libs", "__version__")
+
+from . import _version
+__version__ = _version.get_versions()['version']

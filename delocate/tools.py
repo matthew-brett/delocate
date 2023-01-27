9816d453f154c2ce567a8697de528adc451ef636
Sam Gross <colesbury@gmail.com>
2023-01-27 12:18:15 -0500

    Use frozenset for MACHO_MAGIC

--- delocate/tools.py
+++ delocate/tools.py
@@ -151,13 +151,15 @@ def _run(
 # Mach-O magic numbers. See:
 # https://github.com/apple-oss-distributions/xnu/blob/5c2921b07a2480ab43ec66f5b9e41cb872bc554f/EXTERNAL_HEADERS/mach-o/loader.h#L65
 # https://github.com/apple-oss-distributions/cctools/blob/658da8c66b4e184458f9c810deca9f6428a773a5/include/mach-o/fat.h#L48
-MACHO_MAGIC = (
-    0xFEEDFACE.to_bytes(4, "little"),  # MH_MAGIC
-    0xFEEDFACE.to_bytes(4, "big"),  # MH_MAGIC
-    0xFEEDFACF.to_bytes(4, "little"),  # MH_MAGIC_64
-    0xFEEDFACF.to_bytes(4, "big"),  # MH_MAGIC_64
-    0xCAFEBABE.to_bytes(4, "big"),  # FAT_MAGIC (always big-endian)
-    0xCAFEBABF.to_bytes(4, "big"),  # FAT_MAGIC_64 (always big-endian)
+MACHO_MAGIC = frozenset(
+    [
+        0xFEEDFACE.to_bytes(4, "little"),  # MH_MAGIC
+        0xFEEDFACE.to_bytes(4, "big"),  # MH_MAGIC
+        0xFEEDFACF.to_bytes(4, "little"),  # MH_MAGIC_64
+        0xFEEDFACF.to_bytes(4, "big"),  # MH_MAGIC_64
+        0xCAFEBABE.to_bytes(4, "big"),  # FAT_MAGIC (always big-endian)
+        0xCAFEBABF.to_bytes(4, "big"),  # FAT_MAGIC_64 (always big-endian)
+    ]
 )
 
 

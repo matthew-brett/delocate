6211b2570714a214a29d2045855aaca73afa4160
Kyle Benesch <4b796c65+github@gmail.com>
2023-05-22 03:49:47 -0700

    Replace versioneer with setuptools-scm, migrate to pyproject.toml.
    Versioneer has been completely removed.
    Now `_version.py` is generated during setup, but I've tweaked the script
    so that the package can run even without it.
    
    Setup metadata has been moved into pyproject.toml as well.

--- delocate/__init__.py
+++ delocate/__init__.py
@@ -1,11 +1,12 @@
 # Init for delocate package
 
-from . import _version
 from .delocating import delocate_path, delocate_wheel, patch_wheel
 from .libsana import tree_libs, wheel_libs
 
-__version__ = _version.get_versions()["version"]
-del _version
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
 
 __all__ = (
     "delocate_path",
--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python
 """ setup script for delocate package """
-import codecs
 from os.path import join as pjoin
 
-import versioneer
 from setuptools import find_packages, setup
 
 setup(
-    name="delocate",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Move macOS dynamic libraries into package",
-    author="Matthew Brett",
-    maintainer="Matthew Brett",
-    author_email="matthew.brett@gmail.com",
-    url="http://github.com/matthew-brett/delocate",
     packages=find_packages(),
-    python_requires=">=3.7",
-    install_requires=[
-        "machomachomangler; sys_platform == 'win32'",
-        "bindepend; sys_platform == 'win32'",
-        "packaging>=20.9",
-        "typing_extensions",
-    ],
     package_data={
         "delocate.tests": [
             pjoin("data", "*.dylib"),
@@ -41,7 +24,7 @@ setup(
     },
     entry_points={
         "console_scripts": [
-            "delocate-{} = delocate.cmd.delocate_{}:main".format(name, name)
+            f"delocate-{name} = delocate.cmd.delocate_{name}:main"
             for name in (
                 "addplat",
                 "fuse",
@@ -52,16 +35,4 @@ setup(
             )
         ]
     },
-    license="BSD license",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Environment :: Console",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Operating System :: MacOS :: MacOS X",
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Software Development :: Build Tools",
-    ],
-    long_description=codecs.open("README.rst", "r", encoding="utf-8").read(),
 )

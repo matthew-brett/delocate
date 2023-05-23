8d6325a30aa711c55fea75d038aeb86fd6f8d3df
Kyle Benesch <4b796c65+github@gmail.com>
2023-05-23 03:48:14 -0700

    Warn when delocate is run without version metadata.

--- delocate/__init__.py
+++ delocate/__init__.py
@@ -1,5 +1,7 @@
 # Init for delocate package
 
+import warnings
+
 from .delocating import delocate_path, delocate_wheel, patch_wheel
 from .libsana import tree_libs, wheel_libs
 
@@ -7,6 +9,12 @@ try:
     from ._version import __version__
 except ImportError:
     __version__ = "0.0.0"
+    warnings.warn(
+        "Delocate was not installed and is missing version metadata."
+        "\nMake sure this package is installed in development mode"
+        " with this command:\n\tpip install --editable .",
+        RuntimeWarning,
+    )
 
 __all__ = (
     "delocate_path",

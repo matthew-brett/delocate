8faed8b152aa545ccf81cca11ded2a0bcaddb225
Jeremy Volkman <jeremy@cedarai.com>
2024-02-05 20:01:32 -0800

    Tweaks for Windows portability
    Don't assume we're running on a system with POSIX-style paths.

--- delocate/libsana.py
+++ delocate/libsana.py
@@ -9,6 +9,7 @@ import sys
 import warnings
 from os.path import basename, dirname, realpath
 from os.path import join as pjoin
+from pathlib import Path
 from typing import (
     Callable,
     Dict,
@@ -40,7 +41,11 @@ class DependencyNotFound(Exception):
     """
 
 
-def _filter_system_libs(libname: Text) -> bool:
+def filter_system_libs(libname: Text) -> bool:
+    """Return True if libname starts with /System or /usr/lib."""
+    # If libname has a drive letter, we need to strip it off first.
+    _, libname = os.path.splitdrive(libname)
+    libname = Path(libname).as_posix()
     return not (libname.startswith("/usr/lib") or libname.startswith("/System"))
 
 
@@ -91,7 +96,7 @@ def get_dependencies(
         logger.debug("Ignoring dependencies of %s" % lib_fname)
         return
     if not os.path.isfile(lib_fname):
-        if not _filter_system_libs(lib_fname):
+        if not filter_system_libs(lib_fname):
             logger.debug(
                 "Ignoring missing library %s because it is a system library.",
                 lib_fname,
@@ -111,7 +116,7 @@ def get_dependencies(
             else:
                 dependency_path = search_environment_for_lib(install_name)
             if not os.path.isfile(dependency_path):
-                if not _filter_system_libs(dependency_path):
+                if not filter_system_libs(dependency_path):
                     logger.debug(
                         "Skipped missing dependency %s"
                         " because it is a system library.",
@@ -327,7 +332,7 @@ def _tree_libs_from_libraries(
 def tree_libs_from_directory(
     start_path: str,
     *,
-    lib_filt_func: Callable[[str], bool] = _filter_system_libs,
+    lib_filt_func: Callable[[str], bool] = filter_system_libs,
     copy_filt_func: Callable[[str], bool] = lambda path: True,
     executable_path: Optional[str] = None,
     ignore_missing: bool = False,
@@ -754,7 +759,7 @@ def wheel_libs(
         When dependencies can not be located and `ignore_missing` is False.
     """
     if filt_func is None:
-        filt_func = _filter_system_libs
+        filt_func = filter_system_libs
     with TemporaryDirectory() as tmpdir:
         zip2dir(wheel_fname, tmpdir)
         lib_dict = tree_libs_from_directory(
